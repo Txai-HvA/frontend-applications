d1d5a514652677becc80c754340c9ef5d2a2bd4a
Txai-HvA <txai.pereira.de.lacerda@hva.nl>
2021-12-01 22:43:29 +0100

    Split up functions. Started with artistInfo popup

--- src/components/LastFMData/TopArtists.jsx
+++ src/components/LastFMData/TopArtists.jsx
@@ -1,170 +1,228 @@
 import React, { useState, useEffect } from 'react';
-import * as d3 from "d3";
-import '../../css/TopArtists.css'
-import ColorHash from 'color-hash'
+import * as d3 from 'd3';
+import '../../css/TopArtists.css';
+import ColorHash from 'color-hash';
 
 export const TopArtists = ({ apiKey, userName, limit, period }) => {
-  const [lastFMData, updatelastFMData] = useState({});
-  let colorHash = new ColorHash();
-  
-  useEffect(() => {
-    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=${userName}&api_key=${apiKey}
+    const [topsingleArtistData, updateTopsingleArtistData] = useState({});
+    const [singleArtistData, updateSingleArtist] = useState({});
+    let colorHash = new ColorHash();
+    let topArtists = [];
+
+    //Gets top artists data with the given username, limit, period and apiKey
+    useEffect(() => {
+        fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=${userName}&api_key=${apiKey}
       &limit=${limit}&period=${period}&nowplaying=true&format=json`)
-      .then(response => {
-        if (response.ok) {//Checks whether the HTTP response is okay
-          return response.json();//Extract the JSON from the response
-        }
-        throw new Error('error');
-      })
-      .then(data => updatelastFMData(data))
-      .catch(() =>
-        updatelastFMData({ error: 'Whoops! Something went wrong with Last.fm' })
-      );
-  }, []);
+            .then((response) => {
+                if (response.ok) {
+                    //Checks whether the HTTP response is okay
+                    return response.json(); //Extract the JSON from the response
+                }
+                throw new Error('error');
+            })
+            .then((data) => updateTopsingleArtistData(data))
+            .catch(() =>
+                updateTopsingleArtistData({
+                    error: 'Whoops! Something went wrong with Last.fm',
+                })
+            );
+    }, [apiKey, userName, limit, period]);
 
-  const createPieChart = () => {
-    const { error } = lastFMData;
+    //Gets a single artist data
+    const getSingleArtist = (artist) => {
+        fetch(
+            `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`
+        )
+            .then((response) => {
+                if (response.ok) {
+                    //Checks whether the HTTP response is okay
+                    return response.json(); //Extract the JSON from the response
+                }
+                throw new Error('error');
+            })
+            .then((data) => {
+                updateSingleArtist(data.artist);
+            })
+            .catch(() =>
+                updateSingleArtist({
+                    error: 'Whoops! Something went wrong with Last.fm',
+                })
+            );
+    };
 
-    if (error) {
-      return <p>{error}</p>;
-    } else {
-      const topArtists = lastFMData?.topartists?.artist;
+    //Creates a pie chart
+    const createPieChart = () => {
+        //Creates the pie chart
+        const createGraph = (topArtists) => {
+            let width = 500;
+            let height = 500;
+            //The radius of the pieplot is half the width or half the height (smallest one)
+            let outerRadius = Math.min(width, height) / 2;
 
-      if (!topArtists) {
-        return <h2>Loading artists data... ⏳</h2>;
-      }
+            //Removes the old svg
+            d3.select('#userArtistPieChart').select('svg').remove();
 
-      let topArtistsNew = [];
+            //Creates new svg
+            const svg = d3
+                .select('#userArtistPieChart')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .append('g')
+                .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-      topArtists.forEach(d => {
-        topArtistsNew.push({
-          artistName: d.name,
-          value: d.playcount,
-          url: d.url,
-          image: d.image[2]["#text"]
-        })
-      });
+            //Creates the circle
+            //where .innerRadius() is the hole in the middle
+            //and where .outerRadius() is the actual radius of the circle
+            const arcGenerator = d3
+                .arc()
+                .innerRadius(90)
+                .outerRadius(outerRadius);
 
-      //Creates the pie chart
-      const createGraph = (topArtistsNew) => {
-        let width = 500;
-        let height = 500;
-        //The radius of the pieplot is half the width or half the height (smallest one)
-        let outerRadius = Math.min(width, height) / 2;
-  
-        //Removes the old svg
-        d3.select('#userArtistPieChart')
-          .select('svg')
-          .remove();
-        
-        //Creates new svg
-        const svg = d3
-          .select('#userArtistPieChart')
-          .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-              .attr('transform', `translate(${width / 2}, ${height / 2})`);
-  
-        //Creates the circle
-        //where .innerRadius() is the hole in the middle
-        //and where .outerRadius() is the actual radius of the circle
-        const arcGenerator = d3.arc().innerRadius(90).outerRadius(outerRadius);
-  
-        const pieGenerator = d3
-          .pie()
-          .padAngle(0)
-          .value((d) => d.value);
-  
-        const userArtistPieChartSVG = svg
-          .selectAll()
-          .data(pieGenerator(topArtistsNew))
-          .enter();
+            const pieGenerator = d3
+                .pie()
+                .padAngle(0)
+                .value((d) => d.value);
 
-        //Append arcs
-        userArtistPieChartSVG
-          .append('path')
-            .style('fill', (d, i) => colorHash.hex(d.data.artistName))
-              .transition()
+            const userArtistPieChartSVG = svg
+                .selectAll()
+                .data(pieGenerator(topArtists))
+                .enter();
+
+            //Append arcs
+            userArtistPieChartSVG
+                .append('path')
+                .style('fill', (d, i) => colorHash.hex(d.data.artistName))
+                .transition()
                 .delay((d, i) => i * 500)
-              .attr('d', arcGenerator)
-              .style("opacity", "1");
+                .attr('d', arcGenerator);
 
-        //Tooltip?
-        const path = userArtistPieChartSVG.selectAll('path');
-        path.on("mouseover", (i, d) => {
-          // console.log(d.data.artistName)
+            //Append text labels
+            userArtistPieChartSVG
+                .append('text')
+                .transition()
+                .delay((d, i) => i * 500)
+                .attr('text-anchor', 'middle')
+                .attr('alignment-baseline', 'middle')
+                .text((d) => d.data.value)
+                .style('fill', (d, i) => {
+                    let artistColor = colorHash.hex(d.data.artistName);
+                    artistColor = invertColor(artistColor);
+                    return artistColor;
+                })
+                .attr(
+                    'transform',
+                    (d) => `translate(${arcGenerator.centroid(d)})`
+                ); //used to compute the midpoint of the centerline of the arc
+        };
 
-        });
-        
-        //Append text labels
-        userArtistPieChartSVG
-          .append('text')
-            .transition()
-              .delay((d, i) => i * 500)
-            .attr('text-anchor', 'middle')
-            .attr('alignment-baseline', 'middle')
-            .text((d) => d.data.value)
-              .style('fill', (d, i) => {
-                let artistColor = colorHash.hex(d.data.artistName);
-                artistColor = invertColor(artistColor);
-                return artistColor;
-              })
-              .attr('transform', (d) => `translate(${arcGenerator.centroid(d)})`);//used to compute the midpoint of the centerline of the arc
-      }
+        //Inverts the color of the given hex
+        const invertColor = (hex) => {
+            if (hex.indexOf('#') === 0) {
+                hex = hex.slice(1);
+            }
+            // convert 3-digit hex to 6-digits.
+            if (hex.length === 3) {
+                hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+            }
+            // invert color components
+            let r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+                g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+                b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+            // pad each with zeros and return
+            return '#' + padZero(r) + padZero(g) + padZero(b);
+        };
 
-      //Inverts the color of the given hex
-      const invertColor = (hex) => {
-        if (hex.indexOf('#') === 0) {
-            hex = hex.slice(1);
+        //Adds 0's if needed
+        const padZero = (str, len) => {
+            len = len || 2;
+            let zeros = new Array(len).join('0');
+            return (zeros + str).slice(-len);
+        };
+        //Source https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
+
+        if (topArtists.length > 0) {
+            //Wait 1 second till the graph can generate
+            setTimeout(function () {
+                createGraph(topArtists);
+            }, 1000);
         }
-        // convert 3-digit hex to 6-digits.
-        if (hex.length === 3) {
-            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-        }
-        // invert color components
-        let r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
-            g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
-            b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
-        // pad each with zeros and return
-        return '#' + padZero(r) + padZero(g) + padZero(b);
-      }
-  
-      //Adds 0's if needed
-      const padZero = (str, len) => {
-        len = len || 2;
-        let zeros = new Array(len).join('0');
-        return (zeros + str).slice(-len);
-      }
-      //Source https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
+    };
 
-      if(topArtistsNew.length > 0) {
-        //Wait 1 second till the graph can generate
-        setTimeout(function() {
-          createGraph(topArtistsNew);
-        }, 1000);
-      }
-            
-      return (<div id="userArtistContainer"> 
+    //Gets the data value thats going to be used
+    const filterData = () => {
+        const { error } = topsingleArtistData;
+
+        if (error) {
+            return <p>{error}</p>;
+        } else {
+            if (!topsingleArtistData?.topartists?.artist) {
+                return <h2>Loading artists data... ⏳</h2>;
+            }
+
+            topsingleArtistData?.topartists?.artist.forEach((d) => {
+                topArtists.push({
+                    artistName: d.name,
+                    value: d.playcount,
+                    url: d.url,
+                    image: d.image[2]['#text'],
+                });
+            });
+        }
+    };
+
+    const getArtistInfo = (artistName) => {
+        getSingleArtist(artistName);
+        const artistInfo = d3.select('#artistInfo');
+        artistInfo.style('visibility', 'visible');
+        artistInfo.select('p').html(singleArtistData?.bio?.summary);
+    };
+
+    const hideArtistInfo = () => {
+        const artistInfo = d3.select('#artistInfo');
+        artistInfo.style('visibility', 'hidden');
+    };
+
+    const createComponent = () => {
+        filterData();
+        createPieChart();
+
+        return (
+            <div id="userArtistContainer">
                 <div>
-                  <h3>Top Artists 🎙️</h3>
-                  <div id="userArtistPieChart" />
+                    <h3>Top Artists 🎙️</h3>
+                    <div id="userArtistPieChart" />
                 </div>
                 <ul id="userArtistLegend">
-                  {topArtistsNew.map((d, i) => {
-                    return (<li>
-                              <a href={d.url}>
-                                <span style={{color: "transparent", textShadow: `0 0 0 ${colorHash.hex(d.artistName)}`}}>🎙️</span> #{i+1} {d.artistName} 
-                              </a>
-                            </li>)
-                  })}
+                    {topArtists.map((d, i) => {
+                        return (
+                            <li>
+                                <button
+                                    onClick={() => getArtistInfo(d.artistName)}
+                                >
+                                    <span
+                                        style={{
+                                            color: 'transparent',
+                                            textShadow: `0 0 0 ${colorHash.hex(
+                                                d.artistName
+                                            )}`,
+                                        }}
+                                    >
+                                        🎙️
+                                    </span>{' '}
+                                    #{i + 1} {d.artistName}
+                                </button>
+                            </li>
+                        );
+                    })}
                 </ul>
-              </div>);
-    }    
-  };
+                <div id="artistInfo">
+                    <button onClick={() => hideArtistInfo()}>X</button>
+                    <p></p>
+                </div>
+            </div>
+        );
+    };
 
-  return createPieChart();
+    return createComponent();
 };
-
-
-
